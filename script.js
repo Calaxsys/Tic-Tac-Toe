5a0399c6babadd411bf9690eb1415fe5625469f3
Chris <ffson1999@yahoo.com>
2024-05-31 19:50:59 -0400

    Finished CSS for basic layout

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
-const Gameboard = {
-  gameboard: []
+const Gameboard = (() => {
+  let gameboard = ["", "", "", "", "", "", "", "", ""];
+
+  const render = () => {
+    let boardDisplay = "";
+    gameboard.forEach((square, index) => {
+      boardDisplay += `<div class="square" id="square-${index}">${square}</div>`
+    })
+    document.querySelector("#gameboard").innerHTML = boardDisplay;
+  }
+  
+
+  return {
+    render
+  }
+})();
+
+//mark represents the players X or O
+const createPlayer = (name, mark) => {
+  return {
+    name, 
+    mark
+  }
 }
+
+const Game = (() => {
+  let players = [];
+  let currentPlayerIndex;
+  let gameOver;
+
+  const start = () => {
+    players = [
+      createPlayer(document.querySelector("#player1").value, "X"),
+      createPlayer(document.querySelector("#player2").value, "O")
+    ]
+    currentPlayerIndex = 0;
+    gameOver = false;
+    Gameboard.render();
+  }
+
+  return {
+    start
+  }
+})();
+
+const startButton = document.querySelector("#start-game");
+startButton.addEventListener("click", () => {
+  Game.start();
+})
